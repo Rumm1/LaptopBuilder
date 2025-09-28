fb0ea336a9465f12d3afbc62eaa3f3ba5f2d1e51
Rumm1 <241705@astanait.edu.kz>
2025-09-28 21:56:56 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        LaptopDirector director = new LaptopDirector();
+        LaptopFactory gamingFactory = new GamingLaptopFactory();
+        Laptop businessLaptop = new BusinessLaptopFactory().createLaptop();
+        System.out.println("Gaming: " + gamingFactory.createLaptop());
+        System.out.println("Business: " + businessLaptop);
+        LaptopFactoryAbstract dell = new DellFactory();
+        LaptopFactoryAbstract asus = new AsusFactory();
+        System.out.println("Dell Gaming: " + dell.createGamingLaptop());
+        System.out.println("Asus Business: " + asus.createBusinessLaptop());
 
-        Laptop gamingLaptop = director.createGamingLaptop();
-        Laptop ultrabook = director.createUltrabook();
-        Laptop workstation = director.createWorkstationLaptop();
+        LegacyLaptop oldDell = new LegacyLaptop("Dell", "Inspiron", "Intel P4", 2048, 160);
+        LegacyLaptop oldThinkpad = new LegacyLaptop("IBM", "ThinkPad", "Core 2 Duo", 4096, 320);
 
-        System.out.println(gamingLaptop);
-        System.out.println(ultrabook);
-        System.out.println(workstation);
+        oldDell.displayLegacyInfo();
+        oldThinkpad.displayLegacyInfo();
+
+        Laptop adaptedDell = new LegacyLaptopAdapter(oldDell)
+                .withGPU("Intel GMA 950")
+                .withScreenSize(15.4)
+                .build();
+        System.out.println("Adapted: " + adaptedDell);
+        Laptop modern = new LaptopDirector().createGamingLaptop();
+        Laptop adapted = new LegacyLaptopAdapter(
+                new LegacyLaptop("Alienware", "Area-51m", "i7", 16384, 1000))
+                .withGPU("GTX 1080")
+                .build();
+
+        processLaptop(modern);
+        processLaptop(adapted);
+    }
+    private static void processLaptop(Laptop laptop) {
+        System.out.println("Processing: " + laptop);
     }
 }
